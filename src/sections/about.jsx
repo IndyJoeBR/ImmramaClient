b275c8b52736531161b5154975141fb629b4d571
IndyJoe <IndyJoeBRJS@gmail.com>
2020-12-01 21:22:23 -0500

    another

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -24,13 +24,13 @@ class About extends React.Component {
 
                 <p>Essentially these are stories about adventure. The Irish tale which is probably the most accessible to the modern reader would be The Navagatio... the Voyage of St. Brendan the Navigator. (I find that The Odyssey is very similar in structure, although Odysseus was just trying to get home to Ithica after the Trojan War.)</p>
 
-                <h4 className="aboutHeadlines" >What is the connection between an old Irish tale of adventure and this site?</h4>
+                <h4>What is the connection between an old Irish tale of adventure and this site?</h4>
 
                 <p>While most people who travel now fly, that is actually very similar to a sea voyage. The (air) ship still has a captain and there are passengers. (And there is certainly still 'steerage' for the majority of us.) And we still travel from place to place. However, we are now rarely on a quest or seeking adventure; although... maybe we should be.</p>
 
                 <p>Instead of writing a travel blog, I would like you to share a story about your adventures while you were traveling. Tell us where you went and why and then something interesting that happened. Attach a picture or a video or both and share your adventure with others.</p>
 
-                <p>Not a traveller?  Maybe you have another journey you have been on.  Starting a new job.  Loosing weight.  Two weeks in quarantine and what you did to amuse yourself.  Learning to play an instrument.  Those are all a kind of journey.  You can share those stories as well.</p>
+                <p>Not a traveller?  Maybe you have another journey you have been on.  Starting a new job.  Loosing weight.  Two weeks in quarantine and what you did to occupy your time.  Learning to play an instrument.  Those are all a kind of journey.  You can share those stories as well.</p>
 
                 <h4 className="aboutHeadlines" >What is your immram?</h4>
 
